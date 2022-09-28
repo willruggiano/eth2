4077c54913578a78297c3e14df48fd29d4ede6d8
Will Ruggiano <wmruggiano@gmail.com>
2022-09-28 17:05:12 -0600

    fix: mothership access to secrets

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,15 @@
 let
-  willruggiano = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEAIngPgrqRfYi/YTrd0+eVRbylSL+weBTtL819GgXUb";
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACSzX6qdR3gm7bvXq8f6e8bVhKc2VFEgzXNC2p4jYnk";
+  developers = [
+    # willruggiano (ecthelion)
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEAIngPgrqRfYi/YTrd0+eVRbylSL+weBTtL819GgXUb"
+    # willruggiano (mothership)
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERAQpJ3mjcz+b2Y+Wf598wURIrGU710Sr91HCcwSiXS"
+  ];
+
+  systems = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACSzX6qdR3gm7bvXq8f6e8bVhKc2VFEgzXNC2p4jYnk"
+  ];
 in {
-  "prysm-env.age".publicKeys = [willruggiano system];
-  "wallet-password.age".publicKeys = [willruggiano system];
+  "prysm-env.age".publicKeys = developers ++ systems;
+  "wallet-password.age".publicKeys = developers ++ systems;
 }
